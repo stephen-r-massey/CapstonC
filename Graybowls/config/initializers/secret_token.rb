9003afec4f3327f4b1692625c2b2d1f602b145f1
Stephen <stephen@Ryan.(none)>
2013-01-28 15:11:44 -0600

    initial import

---
+++ Graybowls/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graybowls::Application.config.secret_token = 'e56197aa6cbc7d90ff59a712019c1fcc910ba094add549029f711a03a06a2db8645e3b0888e105f52c3dbf6b135e2208a9a7c211a4ea7af46dc9a20a3e35a165'
